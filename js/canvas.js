3b4dcd200edf294ef83c0fba50c02bb3da471b24
gbarnesus <george.barnesii@yahoo.com>
2015-11-28 07:40:01 -0800

    fully functional

--- js/canvas.js
+++ js/canvas.js
@@ -8,20 +8,22 @@ var contLength = 0;
 var xval = 0;
 var yval = 0;
 var area = 0;
-var imageObj = new Image();
-imageObj.src = 'img/plans-0.png';
-var viewportWidth = $window.width;
-var viewportHeight = $window.height;
+var scale;
+
 ///draw image on canvas
+var image = document.getElementById("source");
+var callback = function(img) {
+  if (!img) img = this;
+  context.imageSmoothingEnabled = false;
+  context.drawImage(image, 0,0, 1300, 841);
 
-      var imageObj = new Image();
-      imageObj.src = 'img/plans-0.png';
-
-      imageObj.onload = function() {
-        context.drawImage(imageObj, 0, 0);
-      };
-
-
+}
+if(image.complete){
+  callback(image);
+}
+else {
+  image.onload = callback;
+}
 
 /// selected button
 $button.click(function() {
@@ -30,8 +32,6 @@ $button.click(function() {
 $(this).addClass("selected");
 });
 
-
-
 /// varibles for click cordinates
 var x
 var y
@@ -39,18 +39,29 @@ var x2
 var y2
 var user_cord = [];
 // set width and height of canvas
-function resizeCanvas() {
+var viewportHeight = 841;
+var viewportWidth = 1300;
+$canvas.attr("height", viewportHeight);
+$canvas.attr("width", viewportWidth);
+var image = document.getElementById("source");
 
-$canvas.attr("width", viewportHeight);
-$canvas.attr("height", viewportWidth);
-}
-// when browser is first open
-resizeCanvas();
-//on window resize
-$window.resize(function() {
-resizeCanvas();
+//notify to set scale
+if (scale == null){
+alert("You need to set scale. Click Scale button to callibrate");
+};
+$(".scale").click(function(){
+alert("Please select two points with known distance");
 
 });
+
+
+// when browser is first open
+//resizeCanvas();
+//on window resize
+//$window.resize(function() {
+//resizeCanvas();
+
+//});
 
 // lengthTool
 $canvas.click(function(e) {
@@ -73,7 +84,7 @@ var xs = user_cord[1].xcord - user_cord[0].xcord;
  var ys = user_cord[1].ycord - user_cord[0].ycord;
  ys = ys * ys;
  length += Math.sqrt(xs + ys)
-console.log(length);
+console.log(length / scale);
  user_cord = [];
 }; //end of if statement to print line and add length
 }/* end of lengthTool*/ else if ($(".contLength").hasClass("selected")) {
@@ -101,6 +112,7 @@ console.log(length);
    ys = ys * ys;
 
    contLength += Math.sqrt(xs + ys);
+   console.log(contLength / scale);
    };
 
 } else if ($(".areaTool").hasClass("selected")) {
@@ -123,7 +135,40 @@ console.log(length);
    };
 
 
-}///end of area tool;
+
+
+
+}
+if ($(".scale").hasClass("selected")) {
+  var parentOffset = $(this).offset();
+   x = e.offsetX;
+   y = e.offsetY;
+var obj = {
+  xcord: x,
+  ycord: y
+}// end of var obj
+user_cord.push(obj);
+context.beginPath();
+if (user_cord.length == 2) {
+context.moveTo(user_cord[0].xcord, user_cord[0].ycord);
+context.lineTo(user_cord[1].xcord, user_cord[1].ycord);
+context.stroke();
+var xs = user_cord[1].xcord - user_cord[0].xcord;
+ xs = xs * xs;
+ var ys = user_cord[1].ycord - user_cord[0].ycord;
+ ys = ys * ys;
+ length += Math.sqrt(xs + ys)
+ console.log(length)
+var user_length = prompt("What is the length of that line?")
+user_length = parseInt(user_length);
+scale = length / user_length
+console.log("user_length:" + user_length);
+console.log("length" + length);
+console.log(scale);
+length = 0;
+user_cord = [];
+}
+}
 });//end of clcik function MAIN
 $($canvas).dblclick(function() {
   if($(".contLength").hasClass("selected")) {
@@ -137,16 +182,16 @@ event.preventDefault();
   context.stroke();
   context.fillstyle = "red";
   context.fill();
-
+var finalcnx = user_cord[0];
+user_cord.push(finalcnx);
 for (i=1; i < user_cord.length; i++) {
   y = i - 1;
-xval += user_cord[y].xcord * user_cord[i].ycord;
-yval += user_cord[i].xcord * user_cord[y].ycord;
-}
-console.log(xval);
-console.log(yval);
-area = (xval - yval) / 2
-console.log(area)
+xval += (user_cord[y].xcord / scale) * (user_cord[i].ycord / scale);
+yval += (user_cord[i].xcord /scale) * (user_cord[y].ycord / scale);
+area = xval - yval;
+};
+area = area / 2;
+console.log(area);
 xval = 0;
 yval = 0;
 area = 0;
